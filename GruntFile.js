3c7ca48b569e2c05509dc2c5fa47a5e39fc5d241
Daniel G <danigdcprojects@gmail.com>
2018-01-03 21:33:58 +0100

    Changed concat to /docs folder

--- GruntFile.js
+++ GruntFile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
             server: {
                 options: {
                     port: 8080,
-                    base: './deploy'
+                    base: './docs'
                 }
             }
         },
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
                 src: [  "src/lib/**/*.js",
                     "src/game/**/*.js"
                      ],
-                dest: 'deploy/js/<%= pkg.name %>.js'
+                dest: 'docs/js/<%= pkg.name %>.js'
             }
         },
         watch: {
